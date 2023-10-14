57c172d5477fb69651561a17dce9dc7152ad8090
Rafikk98 <rafikk1998@gmail.com>
2023-10-15 01:05:26 +0600

    added gitignore

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gf4ueyo.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = "mongodb+srv://rafikk1998:bqrXTIYWwPDsjeUM@cluster0.gf4ueyo.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority`;
+
 console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
